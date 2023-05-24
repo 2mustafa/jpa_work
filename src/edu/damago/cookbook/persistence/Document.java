e2457f4ed5a876f8e4f8338a050d34756e28174c
bWFtdW4 <128370816+bWFtdW4@users.noreply.github.com>
2023-05-24 14:41:43 +0200

    update

--- src/edu/damago/cookbook/persistence/Document.java
+++ src/edu/damago/cookbook/persistence/Document.java
@@ -6,63 +6,66 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
+
 public class Document extends BaseEntity {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long documentIdentity;
-	
-	@Column(nullable = false, updatable =  true) 
+
+	@Column(nullable = false, updatable = true)
 	@Size(min = 64, max = 64)
 	private String hash;
-	
+
 	@Column(nullable = false, updatable = true)
 	private String type;
-	
+
 	@Column(nullable = false, updatable = true)
 	private byte content;
-	
-/**	
-	 PRIMARY KEY (documentIdentity),
-	 FOREIGN KEY (documentIdentity) REFERENCES BaseEntity (identity) ON DELETE CASCADE ON UPDATE CASCADE,
-	 UNIQUE KEY (hash)
-**/
+
+
 	public Document () {
-	
+
 	}
 
-public long getDocumentIdentity () {
-	return documentIdentity;
-}
 
-protected void setDocumentIdentity (long documentIdentity) {
-	this.documentIdentity = documentIdentity;
-}
+	public long getDocumentIdentity () {
+		return documentIdentity;
+	}
 
-public String getHash () {
-	return hash;
-}
 
-public void setHash (String hash) {
-	this.hash = hash;
-}
+	protected void setDocumentIdentity (long documentIdentity) {
+		this.documentIdentity = documentIdentity;
+	}
 
-public String getType () {
-	return type;
-}
 
-public void setType (String type) {
-	this.type = type;
-}
+	public String getHash () {
+		return hash;
+	}
 
-public byte getContent () {
-	return content;
-}
 
-public void setContent (byte content) {
-	this.content = content;
-}
-	
-	
-	
+	public void setHash (String hash) {
+		this.hash = hash;
+	}
+
+
+	public String getType () {
+		return type;
+	}
+
+
+	public void setType (String type) {
+		this.type = type;
+	}
+
+
+	public byte getContent () {
+		return content;
+	}
+
+
+	public void setContent (byte content) {
+		this.content = content;
+	}
+
 }
